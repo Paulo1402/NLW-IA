8d8d97967e7126a5ef96d6c5d9c93b5a537bbce3
Paulo1402 <paulobenatto02@gmail.com>
2023-09-17 18:54:38 -0300

    convert video to audio using ffmpeg

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FileVideo, Github, Upload } from 'lucide-react'
+import { Github, Wand2 } from 'lucide-react'
 import { Button } from './components/ui/button'
 import { Separator } from './components/ui/separator'
 import { Textarea } from './components/ui/textarea'
@@ -10,6 +10,8 @@ import {
   SelectTrigger,
   SelectValue
 } from './components/ui/select'
+import { Slider } from './components/ui/slider'
+import { VideoInputForm } from './components/video-input-form'
 
 function App() {
   return (
@@ -53,44 +55,27 @@ function App() {
           </p>
         </div>
         <aside className="w-80 space-y-6">
-          <form className="space-y-6">
-            <label
-              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-              htmlFor="video"
-            >
-              <FileVideo className="w-4 h-4" />
-              Selecione um vídeo
-            </label>
-            <input
-              type="file"
-              name="video"
-              id="video"
-              accept="video/mp4"
-              className="sr-only"
-            />
-
-            <Separator />
-
-            <div className="space-y-2">
-              <Label htmlFor="transcription_prompt">
-                Prompt de transcrição
-              </Label>
-              <Textarea
-                id="transcription_prompt"
-                className="h-20 leading-relaxed resize-none"
-                placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
-              />
-            </div>
-
-            <Button className="w-full" type="submit">
-              Carregar vídeo
-              <Upload className="w-4 h-4 ml-2" />
-            </Button>
-          </form>
+          <VideoInputForm />
 
           <Separator />
 
           <form className="space-y-6">
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione um prompt..." />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="title">Título do YouTubek</SelectItem>
+                  <SelectItem value="description">
+                    Descrição do YouTubek
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+
             <div className="space-y-2">
               <Label>Modelo</Label>
 
@@ -102,20 +87,29 @@ function App() {
                   <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
                 </SelectContent>
               </Select>
-              <span className="block text-xs text-muted-foreground italic">
+              <span className="block text-sm text-muted-foreground italic">
                 Você poderá customizar essa opção em breve
               </span>
             </div>
 
             <Separator />
 
-            <div className="space-y-2">
-              <Label>Modelo</Label>
+            <div className="space-y-4">
+              <Label>Temperatura</Label>
+              <Slider min={0} max={1} step={0.1} />
 
-              <span className="block text-xs text-muted-foreground italic">
-                Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.
+              <span className="block text-sm text-muted-foreground italic leading-relaxed">
+                Valores mais altos tendem a deixar o resultado mais criativo e
+                com possíveis erros.
               </span>
             </div>
+
+            <Separator />
+
+            <Button className="w-full" type="submit">
+              Executar
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
           </form>
         </aside>
       </main>
