cb8c1a901348414e300ade3640a9a6392d92b29b
Paulo1402 <paulobenatto02@gmail.com>
2023-09-17 11:07:34 -0300

    add lucide-react and separtor component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Button } from "./components/ui/button"
+import { Github } from 'lucide-react'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
-   <>
-    <h1>Hello World!</h1>
-    <Button>Teste</Button>
-   </>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com ❤️ no NLW da Rockeatseat
+          </span>
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
 
